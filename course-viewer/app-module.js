60229372221857311d6f5b5c6bff109fb7d4e97f
YG777 <katyake@gamil.com>
2018-01-23 23:30:49 +0000

    indentation

--- course-viewer/app-module.js
+++ course-viewer/app-module.js
@@ -1,32 +1,32 @@
 (function () {
-	'use strict';
-	var appModule = angular.module('courseViewer', []);
+  'use strict';
+  var appModule = angular.module('courseViewer', []);
 
-	//variable check - turn the borders on and off
-	appModule.value('componentBorders', true);
+  //variable check - turn the borders on and off
+  appModule.value('componentBorders', true);
 
-	//iterate throught all the components and add borders and names to the templates 
-	//run function sexecutes as soon as other angular actions are fully loaded
-	//accommodating both inline templates and template files
-	appModule.run(function (componentBorders) {
-		if (componentBorders) {
-			if (appModule._invokeQueue) {
-				appModule._invokeQueue.forEach(item => {
-					if (item[1] == 'component') {
-						var componentName = item[2][0];
-						var componentProperties = item[2][1];
-						if (componentProperties.templateUrl) {
-							var templateUrl = componentProperties.templateUrl;
-							delete componentProperties.templateUrl;
-							componentProperties.template = '<div class="component-borders"><b>' + componentName + '</b><div ng-include="\'' + templateUrl + '\'"></div></div>';
-						} else {
-							var template = '<div class="component-borders">' + componentName + '<div>' + componentProperties.template + '</div></div>';
-							componentProperties.template = template;
-						}
-					}
-				});
-			}
-		}
-	
-	});
+  //iterate throught all the components and add borders and names to the templates 
+  //run function sexecutes as soon as other angular actions are fully loaded
+  //accommodating both inline templates and template files
+  appModule.run(function (componentBorders) {
+    if (componentBorders) {
+      if (appModule._invokeQueue) {
+        appModule._invokeQueue.forEach(item => {
+          if (item[1] == 'component') {
+            var componentName = item[2][0];
+            var componentProperties = item[2][1];
+            if (componentProperties.templateUrl) {
+              var templateUrl = componentProperties.templateUrl;
+              delete componentProperties.templateUrl;
+              componentProperties.template = '<div class="component-borders"><b>' + componentName + '</b><div ng-include="\'' + templateUrl + '\'"></div></div>';
+            } else {
+              var template = '<div class="component-borders">' + componentName + '<div>' + componentProperties.template + '</div></div>';
+              componentProperties.template = template;
+            }
+          }
+        });
+      }
+    }
+
+  });
 })();
