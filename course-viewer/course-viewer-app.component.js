ff3975e989b3f6992cdcb927842b14f06a08ebe8
YG777 <katyake@gamil.com>
2018-01-23 11:59:03 +0000

    created a child component

---
+++ course-viewer/course-viewer-app.component.js
@@ -0,0 +1,9 @@
+(function() {
+  'use strict';
+  angular.module('courseViewer').component('courseViewerApp', {
+    templateUrl: 'course-viewer/course-viewer-app.component.html'
+
+  });
+})();
+  
+  
