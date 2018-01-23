32cd474b8a85ceeb21e161a8209df3210f7b191a
YG777 <katyake@gamil.com>
2018-01-23 20:50:18 +0000

    created components for author info, course heading, course navigator

---
+++ course-viewer/course/course-heading.component.js
@@ -0,0 +1,9 @@
+(function() {
+	  'use strict';
+
+	  angular.module('courseViewer').component('courseList', {
+	  	templateUrl: 'course-viewer/course/course-heading.component.html'
+ 	 });
+})();
+  
+  
