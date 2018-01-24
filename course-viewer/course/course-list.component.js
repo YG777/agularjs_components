127f591765af118723f76ce30a71d1730239110a
YG777 <katyake@gamil.com>
2018-01-24 21:24:55 +0000

    added controllers for course-list and course components and ui-sref for courseId in course-list html template

--- course-viewer/course/course-list.component.js
+++ course-viewer/course/course-list.component.js
@@ -1,6 +1,14 @@
 (function() {
   'use strict';
   angular.module('courseViewer').component('courseList', {
+    controllerAs: vm,
+    controller: function() {
+      var vm = this;
+      //add $oninit hook
+      vm.$oninit = function() {
+        vm.courseId = 200; //random sample
+      };
+    },
     templateUrl: 'course-viewer/course/course-list.component.html'
   });
 })();
--- course-viewer/course/course.component.js
+++ course-viewer/course/course.component.js
@@ -2,6 +2,14 @@
 (function() {
   	'use strict';
   	angular.module('courseViewer').component('course', {
+		binding: {
+			courseId: '<'
+		},
+		controllerAs: 'vm',
+		controller: function(){
+			var vm = this;
+			
+		}
     	templateUrl: 'course-viewer/course/course.component.html'
   	});
 })();
