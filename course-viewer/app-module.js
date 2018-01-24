8bc8eb1dac65275651bdfd8ee68f1d97b48fa02b
YG777 <katyake@gamil.com>
2018-01-24 21:49:13 +0000

    created state for course

--- course-viewer/app-module.js
+++ course-viewer/app-module.js
@@ -49,6 +49,16 @@
       template: '<course-list></course-list>'
     },
     {
+      name: 'course',
+      url: '/course/{courseId}',
+      resolve: {
+        courseId: function ($stateParams) {
+          return $stateParams.courseId;
+        }
+      },
+      template: '<course courseId="$resolve.courseId"></course>'
+    },
+    {
       name: 'authors',
       url: '/authors',
       template: '<author-list></author-list>'
