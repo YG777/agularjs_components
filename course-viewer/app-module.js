13d762865967100e38be1e77e2cb5cce1c47c516
YG777 <katyake@gamil.com>
2018-01-24 20:35:15 +0000

    linked nav items to the state with ui-sref

--- course-viewer/app-module.js
+++ course-viewer/app-module.js
@@ -1,18 +1,18 @@
 (function () {
   'use strict';
-  var appModule = angular.module('courseViewer', []);
+  var appModule = angular.module('courseViewer', ['ui.router']);
 
   //variable check - turn the borders on and off
   appModule.value('componentBorders', true);
 
   //iterate throught all the components and add borders and names to the templates 
-  //run function sexecutes as soon as other angular actions are fully loaded
+  //run function executes as soon as other angular actions are fully loaded
   //accommodating both inline templates and template files
   appModule.run(function (componentBorders) {
     if (componentBorders) {
       if (appModule._invokeQueue) {
         appModule._invokeQueue.forEach(item => {
-          if (item[1] == 'component') {
+          if (item[1] === 'component') {
             var componentName = item[2][0];
             var componentProperties = item[2][1];
             if (componentProperties.templateUrl) {
@@ -28,5 +28,35 @@
       }
     }
 
+  });
+
+  //configure UI Router
+  appModule.config(function($stateProvider){
+    //define states
+    var states = [{
+      name: 'home',
+      url: '',
+      template: '<home></home>'
+    },
+    {
+      name: 'home2',
+      url: '/',
+      template: '<home></home>'
+    },
+    {
+      name: 'courses',
+      url: '/courses',
+      template: '<course-list></course-list>'
+    },
+    {
+      name: 'authors',
+      url: '/authors',
+      template: '<author-list></author-list>'
+    }
+    ];
+    //register states
+    states.forEach(state => {
+      $stateProvider.state(state);
+    });
   });
 })();
